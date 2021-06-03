1d66766cc71f4602c25f56aceda7c4d63c8e5ae8
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-03 20:37:34 +1000

    defined index file for routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,9 @@
-// Comment
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
