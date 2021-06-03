2720371fdc9c04abd3928df59557e454cddc8032
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-03 21:15:03 +1000

    Uncommented importing routes folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 // const session = require('express-session');
 // const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
+ const routes = require('./controllers');
 //Import helpers folder
 //const helpers = require('./utils/helpers');
 
