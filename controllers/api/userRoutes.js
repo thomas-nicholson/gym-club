14693eebc8ad54709621aabdb8bb371dc0141f97
Punkinut <heypunkinut@gmail.com>
2021-06-07 10:48:58 +1000

    Previous commit

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../../models/User');
+const { User, Blog, Stats} = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
--- models/index.js
+++ models/index.js
@@ -1,1 +1,23 @@
-// Comment
+const User = require('./User');
+const Blog = require('./Blog');
+const Stats = require('./Stats');
+
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasOne(Stats, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Stats.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog, Stats };
