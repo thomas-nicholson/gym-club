a85b32e7d79c4bd85115344baa47a7514d98f50d
Punkinut <heypunkinut@gmail.com>
2021-06-08 09:12:41 +1000

    Fixed spelling error in api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes')
 const workoutRoutes = require('./workoutRoutes')
-const statRoutes = require('./statRoutes')
+const statRoutes = require('./statsRoutes')
 
 router.use('/users', userRoutes);
 router.use('/blog', blogRoutes);
