7271fc2911afc7bafe6a5eb459e62d21d8448ac6
Punkinut <heypunkinut@gmail.com>
2021-06-07 20:01:11 +1000

    Seed file is now able to seed comment data

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,6 +4,7 @@ const { User, Blog, Stats, Comment } = require('../models');
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
 const statsData = require('./statsData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -20,6 +21,10 @@ const seedDatabase = async () => {
     const stats = await Stats.bulkCreate(statsData, {
       returning: true,
     });
+
+    const comments = await Comment.bulkCreate(commentData, {
+      returning: true,
+    })
   
     process.exit(0);
   };
