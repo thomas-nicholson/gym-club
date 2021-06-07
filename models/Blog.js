7d89046adffcd82946c85495753b726e51cb1fbe
Punkinut <heypunkinut@gmail.com>
2021-06-07 10:33:29 +1000

    Created blog model

--- models/Blog.js
+++ models/Blog.js
@@ -1,1 +1,43 @@
-// Comment
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+      date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'Blog',
+    }
+  );
+  
+  module.exports = Blog;
