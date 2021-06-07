aea7cb925643c034d82738aefa3e552dc2273fe6
Punkinut <heypunkinut@gmail.com>
2021-06-07 10:48:29 +1000

    Added index.js file in models which states the data relationships

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const User = require('../models/User');
+const { User, Blog, Stats } = require('../models');
 
 const userData = require('./userData.json');
 
