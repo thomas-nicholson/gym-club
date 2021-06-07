faf98b87ae08b25f34cd5435250f64145e076b1d
Punkinut <heypunkinut@gmail.com>
2021-06-07 10:17:25 +1000

    You are now able to seed users

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,17 @@
-// Comment
+const sequelize = require('../config/connection');
+const User = require('../models/User');
+
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    process.exit(0);
+  };
+  
+  seedDatabase();
