0df23072ec0c04c2a9b4a352e57d9fe00dcdd2f4
Punkinut <heypunkinut@gmail.com>
2021-06-08 08:57:39 +1000

    Added /addworkout route which allows uses to add workouts

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog} = require('../../models');
+const { User, Blog } = require('../../models');
 
 router.post('/newpost', async (req, res) => {
     try {
--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,1 +1,18 @@
-// Workout back end routes
+const router = require('express').Router();
+const { User, Workout } = require('../../models');
+
+router.post('/addworkout', async (req, res) => {
+    try {
+        const newWorkout = await Workout.create({
+            title: req.body.title,
+            description: req.body.description,
+            user_id: req.session.user_id
+        })
+        res.status(200).json(newWorkout);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
