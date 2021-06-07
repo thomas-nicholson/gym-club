a2278fdf88bc18e25465e8d3ada43c90bdd04e7d
Punkinut <heypunkinut@gmail.com>
2021-06-08 09:10:04 +1000

    Added /stats route in the api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes')
 const workoutRoutes = require('./workoutRoutes')
+const statRoutes = require('./statRoutes')
 
 router.use('/users', userRoutes);
 router.use('/blog', blogRoutes);
 router.use('/workout', workoutRoutes);
+router.use('/stats', statRoutes);
 
 module.exports = router;
