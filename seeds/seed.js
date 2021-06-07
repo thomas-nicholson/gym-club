83a7fb0915d35f737f9b1155e792f37956244dcf
Punkinut <heypunkinut@gmail.com>
2021-06-07 10:59:58 +1000

    Created blog post seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,11 +2,17 @@ const sequelize = require('../config/connection');
 const { User, Blog, Stats } = require('../models');
 
 const userData = require('./userData.json');
+const blogData = require('./blogData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
   
     const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    const posts = await Blog.bulkCreate(blogData, {
       individualHooks: true,
       returning: true,
     });
