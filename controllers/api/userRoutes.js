6f583ba5c738c5bc5bc84a298084c252f70b96f8
Punkinut <heypunkinut@gmail.com>
2021-06-07 10:09:03 +1000

    Added all routes for signing up, loggin in and logging out

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,1 +1,56 @@
-// Comment
+const router = require('express').Router();
+const User = require('../../models/User');
+
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+      if (!userData) {
+        res.status(400).json({});
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({});
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (error) {
+      res.status(400).json({ error: error.toString() });
+    }
+  });
+
+  router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+
+  module.exports = router;
