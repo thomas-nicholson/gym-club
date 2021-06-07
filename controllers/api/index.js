cef6c8f48f266f5d0b9dd9a83e895b008e6d98fe
Punkinut <heypunkinut@gmail.com>
2021-06-08 08:55:01 +1000

    Setup /workout api route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes')
+const workoutRoutes = require('./workoutRoutes')
 
 router.use('/users', userRoutes);
 router.use('/blog', blogRoutes);
+router.use('/workout', workoutRoutes);
 
 module.exports = router;
