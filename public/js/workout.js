8821249bde9f96a489b9f5eab4f05171751a6b76
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-08 21:31:35 +1000

    added add workout front end javascript functionality. Added update workout front end js function.

---
+++ public/js/workout.js
@@ -0,0 +1,43 @@
+const addWorkoutHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title').value.trim();
+    const description = document.querySelector('#description').value.trim();
+
+    if (title && description) {
+        const response = await fetch('/api/workout/add', {
+            method: 'POST',
+            body: JSON.stringify({ title, description }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+const updateWorkoutHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title').value.trim();
+    const description = document.querySelector('#description').value.trim();
+
+    const id;
+
+    if (title && description) {
+        const response = await fetch('/api/workout/update/'+id, {
+            method: 'PUT',
+            body: JSON.stringify({ title, description }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
