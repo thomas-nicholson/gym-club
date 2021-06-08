8a98a432a0b8e9ea03a7e038fd01a1b363a14c27
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-08 18:50:03 +1000

    Deleted superfluous route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -47,23 +47,13 @@ router.post('/', async (req, res) => {
     }
   });
 
-  router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
-
-router.get('/:userID', async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.params.userID);
-    res.status(200).json(userData);
-
-  } catch (err) {
-    res.status(400).json(err);
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
   }
 });
 
