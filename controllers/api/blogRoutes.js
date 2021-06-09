d8f8ce0817e89716d08c1ec27783e356cc081f8f
Keerbi Liu <kirby.liu12@gmail.com>
2021-06-09 18:22:51 +1000

    Added blogRoutes, tested in insomnia, create new blog, update blog, delete blog all work.

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog } = require('../../models');
+const { User, Blog, Comment } = require('../../models');
 const auth = require('../../utils/auth');
 
 // The structure for this is very similar to workoutRoutes.js
@@ -14,6 +14,47 @@ router.post('/newpost', auth, async (req, res) => {
 
     } catch (err) {
         res.status(400).json(err);
+    }
+});
+
+ // This allows you to update the blogs
+router.put('/update/:id', auth, async (req, res) => {
+    try {
+        const blogUpdate = await Blog.update(
+            {
+                title: req.body.title,
+                description: req.body.description,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                 //   user_id: req.session.user_id
+                },
+            }
+        )
+        res.status(200).json(blogUpdate)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// This allows you to delete blogs
+router.delete('/delete/:id', auth, async (req, res) => {
+    try {
+        const commentDelete = await Comment.destroy({
+            where: {
+                post_id: req.params.id
+            }
+        })
+        const blogDelete = await Blog.destroy({
+            where: {
+                id: req.params.id,
+              //  user_id: 2
+            }
+        })
+        res.status(200).json(blogDelete)
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
