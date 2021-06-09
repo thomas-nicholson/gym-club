845c26d23ab264993f9c4d87280c1247577c796d
Keerbi Liu <kirby.liu12@gmail.com>
2021-06-09 10:36:54 +1000

    Added blog routes.

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -16,4 +16,40 @@ router.post('/newpost', async (req, res) => {
     }
 });
 
+ // This allows you to update the blogs
+router.put('/update/:id', async (req, res) => {
+    try {
+        const blogUpdate = await Blog.update(
+            {
+                title: req.body.title,
+                description: req.body.description,
+                date_created: req.body.date_created,
+                user_id: req.params.id
+            },
+            {
+                where: {
+                    id: req.params.id
+                },
+            }
+        )
+        res.status(200).json(blogUpdate)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// THis allows you to delete blogs
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const blogDelete = await Blog.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(200).json(blogDelete)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
--- controllers/api/statsRoutes.js
+++ controllers/api/statsRoutes.js
@@ -1,4 +1,14 @@
 // This will relate to the users profile page such as updating their age, height and max stats etc
 // The get routes in homeRoutes.js will get the stats the on users profile page due to the foreign key relationship in the database
 const router = require('express').Router();
+const { Stats } = require('../../models');
+
+// router.post('/', async (req, res) => {
+//     try {
+//         const userData = await Stats.create(req.body);
+
+        
+//     }
+// })
+
 module.exports = router;
