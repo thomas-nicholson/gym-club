4d4ce2543da94563bf5aaed3e5e185a58e0a33bf
Punkinut <heypunkinut@gmail.com>
2021-06-11 10:25:21 +1000

    Made sure that you can only edit your icon if it's your account

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -99,12 +99,20 @@ router.get('/user/:id', auth,  async (req, res) => {
                 },
             ],
         });
+
+        let allowIconEdit;
+        if (req.params.id == req.session.user_id) {
+            allowIconEdit = true;
+        } else {
+            allowIconEdit = false;
+        }
     
         const users = user.get({ plain: true});
         res.render('user', {
             ...users,
             logged_in: req.session.logged_in,
-            user_id: req.session.user_id
+            user_id: req.session.user_id,
+            allowIconEdit
         });
     } catch (err) {
         res.status(500).json(err);
@@ -112,7 +120,7 @@ router.get('/user/:id', auth,  async (req, res) => {
     
 });
 
-router.get('/userBlogs/:id', auth,  async (req, res) => {
+router.get('', auth,  async (req, res) => {
     try {
         const userBlogData = await Blog.findAll({
             where: {
