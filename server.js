8d5d0c29903cd151e56c01790736ebc12c642b71
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-14 22:38:11 +1000

    Added middleware to fix images not appearing

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static('public/img'));
+app.use(express.static('public/Images'));
 
 //Linking routes folder to express
 app.use(routes);
