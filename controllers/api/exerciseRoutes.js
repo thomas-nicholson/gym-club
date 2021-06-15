da1b3b9009b1f66a8d2c239f478110c5f0ef2d01
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-15 14:30:37 +1000

    Added security to update exercise

--- controllers/api/exerciseRoutes.js
+++ controllers/api/exerciseRoutes.js
@@ -20,22 +20,32 @@ router.post('/add/:id', auth, async (req, res) => {
 
   // This allows you to update exercises
 router.put('/update/:id', auth, async (req, res) => {
-  try {
-    const exerciseUpdate = await Exercise.update(
-      {
-        exercise: req.body.exercise,
-        reps: req.body.reps,
-        sets: req.body.sets,
-        workout_id: req.params.id
-      },
-      {
-        where: {
-          id: req.params.id
-        },
-      }
-    )
 
-    res.status(200).json(exerciseUpdate)
+  try {
+
+    const exerciseData = await Exercise.findByPk(req.params.id, {include: {model: Workout}});
+    const exercise = exerciseData.get({ plain: true });
+
+    if (exercise.workout.user_id == req.session.user_id) {
+      const exerciseUpdate = await Exercise.update(
+        {
+          exercise: req.body.exercise,
+          reps: req.body.reps,
+          sets: req.body.sets,
+          workout_id: req.params.id
+        },
+        {
+          where: {
+            id: req.params.id
+          },
+        }
+      );
+      res.status(200).json(exerciseUpdate)
+    } else {
+      res.status(200).json({"message": "Could not update exercise"});
+    }
+
+    
   } catch (err) {
     res.status(500).json(err);
   }
