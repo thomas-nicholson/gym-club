ac2dbaa6df8b8d219e5943b42ee67a952b304359
Samuel Heal <samuelheal@hotmail.com>
2021-06-15 15:33:25 +1000

    update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -102,7 +102,6 @@ router.get('/blog/:id', auth,  async (req, res) => {
         })
 
         const haveLiked = liked.map((like) => like.get({ plain: true}))
-        console.log(haveLiked)
 
         let allowEdit;
         if (blog.user_id == req.session.user_id) {
